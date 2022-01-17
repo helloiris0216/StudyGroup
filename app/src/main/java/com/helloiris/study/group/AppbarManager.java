0a6d09f36402c6e59751f4de9e941ed8d2b8613f
Helloiris <helloiris0216@gmail.com>
2022-01-17 18:40:36 +0800

    [新增] custom app bar and can count badges of the shopping cart.

--- app/src/main/java/com/helloiris/study/group/AppbarManager.java
+++ app/src/main/java/com/helloiris/study/group/AppbarManager.java
@@ -1,46 +1,83 @@
 package com.helloiris.study.group;
 
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class ActionbarManager {
-    private static final String _TAG = "ActionbarManager";
-    public static ActionbarManager actionbarManager;
-    private AppCompatActivity context;
+public class AppbarManager {
+    private static final String _TAG = "AppbarManager";
+    private static AppbarManager appbarManager;
     private Toolbar actionBar;
     private Button favorites;
     private Button shoppingCart;
+    private TextView badgesTxt;
+    private int badges = 0;
 
-    private ActionbarManager(AppCompatActivity context) {
-        this.context = context;
+
+    private enum BadgeType {
+        SHOPPING_CART,
+        FAVORITES
     }
 
-    public static ActionbarManager getInstance(AppCompatActivity context) {
-        if (actionbarManager == null) {
-            actionbarManager = new ActionbarManager(context);
+    private AppbarManager() {
+    }
+
+
+    public static synchronized AppbarManager getInstance() {
+        if (appbarManager == null) {
+            appbarManager = new AppbarManager();
         }
 
-        return actionbarManager;
+        return appbarManager;
     }
 
 
-    public void initActionbar() {
-        this.actionBar = this.context.findViewById(R.id.toolbar);
+    public void initActionbar(AppCompatActivity activity) {
+        this.actionBar = activity.findViewById(R.id.toolbar);
 
-        this.context.setSupportActionBar(this.actionBar);
+        activity.setSupportActionBar(this.actionBar);
 
         this.favorites = this.actionBar.findViewById(R.id.favorites);
         this.shoppingCart = this.actionBar.findViewById(R.id.shopping_cart);
+        this.badgesTxt = this.actionBar.findViewById(R.id.badge_shopping_cart);
+
+        if (this.badges == 0) {
+            this.badgesTxt.setVisibility(View.GONE);
+        }
+        else {
+            this.badgesTxt.setVisibility(View.VISIBLE);
+        }
 
         this.favorites.setOnClickListener(v -> {
-            Log.d(_TAG, "onClick: favorites");
+            Log.v(_TAG, "onClick: favorites");
         });
 
         this.shoppingCart.setOnClickListener(v -> {
-            Log.d(_TAG, "onClick: shopping cart");
+            Log.v(_TAG, "onClick: shopping cart");
+
+            this.badges+=20;
+
+            this.addBadge(BadgeType.SHOPPING_CART);
+
         });
+    }
+
+
+    private void addBadge(BadgeType badgeType) {
+        Log.v(_TAG, "addBadge: badges = " + this.badges);
+
+        switch (badgeType) {
+            case FAVORITES:
+                break;
+
+            case SHOPPING_CART:
+                this.badgesTxt.setVisibility(View.VISIBLE);
+                this.badgesTxt.setText(String.valueOf(this.badges));
+                break;
+        }
     }
 }
--- app/src/main/java/com/helloiris/study/group/MainActivity.java
+++ app/src/main/java/com/helloiris/study/group/MainActivity.java
@@ -1,15 +1,9 @@
 package com.helloiris.study.group;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.material.appbar.MaterialToolbar;
 
 /*
  * Package com.helloiris.study.group
@@ -40,9 +34,6 @@ public class MainActivity extends AppCompatActivity {
     // the log tag.
     private final String _TAG = Basic.getClassTag(MainActivity.class);
 
-    // a flag when the device receives the notification data from the Intent.
-    private boolean hasNotificationData = false;
-
 
     /**
      * Called when the activity is starting.
@@ -57,32 +48,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         String logTag = "onCreate";
 
+        AppbarManager.getInstance().initActionbar(this);
 
-//        // create a notification channel for the Android Oreo+
-//        // if you don't, the device won't get the notification in the foreground.
-//        createNotificationChannel();
+
+
+//        Toolbar toolbar = this.findViewById(R.id.toolbar);
+//        Button shoppingCart = toolbar.findViewById(R.id.shopping_cart);
+//        Button favorites = toolbar.findViewById(R.id.favorites);
 //
-//        // get the FCM token.
-//        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(task -> Log.i(_TAG, String.format("%s -> FCM token: %s", logTag, task.getResult())));
-
-//        onNewIntent(getIntent());
-
-
-//        ActionBar actionBar = getActionBar();
-//        actionBar.setCustomView(R.layout.view_actionbar);
-//        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-//        actionBar.setBackgroundDrawable(new ColorDrawable(0xF505050));
+//        shoppingCart.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d(_TAG, "onClick: shopping cart");
+//            }
+//        });
+//
+//        favorites.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d(_TAG, "onClick: favorites");
+//            }
+//        });
     }
 
 
     @Override
     protected void onStart() {
         super.onStart();
-
-        MaterialToolbar toolbar = this.findViewById(R.id.actionbar);
-        this.setSupportActionBar(toolbar);
-
-        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     /**
@@ -93,13 +85,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         Log.d(_TAG, "onResume");
-
-//        // 當 app 在背景接收到通知時，在這邊取得 data，key 要寫通知內的 json key.
-//        if (this.getIntent() == null | this.hasNotificationData) return;
-//        if (this.getIntent().getStringExtra(FCMService.IntentKey.data_title.toString()) != null) {
-//            String dataTitle = this.getIntent().getStringExtra(FCMService.IntentKey.data_title.toString());
-//            Log.v(_TAG, String.format("onResume -> FCM data -> content-value: {data title = %s}", dataTitle));
-//        }
     }
 
 
@@ -109,65 +94,5 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
-
-        this.hasNotificationData = false;
-        Log.d(_TAG, "onStop -> hasNotificationData = " + this.hasNotificationData);
-    }
-
-
-    /**
-     * This is called for activities that set launchMode to "singleTop" in
-     * their package, or if a client used the {@link Intent#FLAG_ACTIVITY_SINGLE_TOP}
-     * flag when calling {@link #startActivity}.  In either case, when the
-     * activity is re-launched while at the top of the activity stack instead
-     * of a new instance of the activity being started, onNewIntent() will be
-     * called on the existing instance with the Intent that was used to
-     * re-launch it.
-     *
-     * <p>An activity can never receive a new intent in the resumed state. You can count on
-     * {@link #onResume} being called after this method, though not necessarily immediately after
-     * the completion this callback. If the activity was resumed, it will be paused and new intent
-     * will be delivered, followed by {@link #onResume}. If the activity wasn't in the resumed
-     * state, then new intent can be delivered immediately, with {@link #onResume()} called
-     * sometime later when activity becomes active again.
-     *
-     * <p>Note that {@link #getIntent} still returns the original Intent.
-     * You can use {@link #setIntent} to update it to this new Intent.
-     *
-     * Parameter:
-     *      @param intent The new intent that was started for the activity.
-     */
-    @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-
-        setIntent(intent);
-
-        // 當 app 在背景接收到通知時，在這邊取得 data，key 要寫通知內的 json key.
-        Log.v(_TAG, String.format("onNewIntent -> FCM data -> content-value: {data title = %s}",
-                intent.getStringExtra(FCMService.IntentKey.data_title.toString())));
-        this.hasNotificationData = true;
-    }
-
-
-    /**
-     * To generate a channel for the notification.
-     */
-    public void createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = getString(R.string.channel_name);
-            String description = getString(R.string.channel_description);
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-
-            NotificationChannel channel = new NotificationChannel(this.getResources().getString(R.string.fcm_channel_id), name, importance);
-            channel.setDescription(description);
-
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
     }
 }
